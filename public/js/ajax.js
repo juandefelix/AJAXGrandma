cdb866b66e38de50d0e4e02f73ca5cfad838c055
juandefelix <juandefelix@yahoo.es>
2014-01-29 21:43:24 -0600

    Update and rename Ajax.md to ajax.js

---
+++ public/js/ajax.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+  $("#form_thing").submit(function(event){
+    event.preventDefault();
+    $.post( "/grandma",$("#form_thing").serialize(), function(response){
+      $("#grandma_says").html(response);
+       });
+    });
+});
+```
